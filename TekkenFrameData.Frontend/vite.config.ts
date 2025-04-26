f82bd36b93501c58feccf99d635c10aa079b3609
RXDCODX <maxxxes73rus@gmail.com>
2025-04-27 00:28:10 +0400

    asd

--- TekkenFrameData.Frontend/vite.config.ts
+++ TekkenFrameData.Frontend/vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig, ProxyOptions } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import plugin from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
-import child_process from "child_process";
-import { env } from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), plugin()],
   server: {
     host: true,
-	port: 3000
+    port: 3000,
   },
   build: {
     outDir: "dist",

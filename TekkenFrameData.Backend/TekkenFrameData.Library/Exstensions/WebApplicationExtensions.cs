cc0714c6c3894899ba6a3f3508736bc4a5cc8797
RXDCODX <88150316+RXDCODX@users.noreply.github.com>
2025-04-27 11:34:22 +0400

    changes a lot (#25)
    * changes a lot
    
    * fix compose
    
    * delete cert
    
    * fix
    
    * asd
    
    * asd
    
    * asd
    
    * asd

---
+++ TekkenFrameData.Backend/TekkenFrameData.Library/Exstensions/WebApplicationExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TekkenFrameData.Library.DB;
+
+namespace TekkenFrameData.Library.Exstensions;
+
+public static class WebApplicationExtensions
+{
+    public static IApplicationBuilder Migrate(this IApplicationBuilder app)
+    {
+        var context = GetAppDbContext(app.ApplicationServices);
+        var logger = GetAppLogger(app.ApplicationServices);
+
+        logger.LogInformation("Начало миграции базы данных.");
+
+        logger.LogInformation("Проверка что база данных существует.");
+        context.Database.EnsureCreated();
+        logger.LogInformation("База данных существует.");
+
+        var pendingMigrations = context.Database.GetPendingMigrations();
+
+        if (pendingMigrations.Any())
+        {
+            logger.LogInformation("Было найдены новые миграции. Применяем.");
+            context.Database.Migrate();
+        }
+        else
+        {
+            logger.LogInformation("Новые миграции отсуствуют.");
+        }
+
+        return app;
+    }
+
+    private static ILogger GetAppLogger(IServiceProvider appApplicationServices)
+    {
+        var logger = appApplicationServices.GetRequiredService<ILogger<AppDbContext>>();
+        return logger;
+    }
+
+    private static DbContext GetAppDbContext(IServiceProvider appApplicationServices)
+    {
+        var scopeFactory = appApplicationServices.GetRequiredService<IServiceScopeFactory>();
+        var serviceScope = scopeFactory.CreateScope();
+        var appDbContext = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        return appDbContext;
+    }
+}

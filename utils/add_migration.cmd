0e9f161729cc0eddd0cd5aacbe3b29638e5304e6
RXDCODX <maxxxes73rus@gmail.com>
2025-08-14 12:38:41 +0400

    asd

--- utils/add_migration.cmd
+++ utils/add_migration.cmd
@@ -1,4 +1,4 @@
 @echo off
 set /p migration_name=Enter migration name: 
-dotnet ef migrations add %migration_name% --startup-project ../TekkenFrameData.Backend/TekkenFrameData.Service/TekkenFrameData.Service.csproj --project ../TekkenFrameData.Backend/TekkenFrameData.Library/TekkenFrameData.Library.csproj
+dotnet ef migrations add %migration_name% --startup-project ../TekkenFrameData.Backend/TekkenFrameData.Watcher/TekkenFrameData.Watcher.csproj --project ../TekkenFrameData.Backend/TekkenFrameData.Library/TekkenFrameData.Library.csproj
 pause
